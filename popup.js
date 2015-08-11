b140ab70d0f342af14844ecc37c9af9225c61984
jknam <jknam@ideabove.com>
2015-08-11 23:39:35 +0900

    [#100866970] Commit modification

--- popup.js
+++ popup.js
@@ -7,8 +7,8 @@ function dumpAlbums(youtubeApiKey) {
             var url = tab.url;
             if (url.indexOf('youtube') > 0 && url.indexOf('v=') > 0) {
                 exist = true;
-                var id = url.substring(url.indexOf('v=') + 2, url.indexOf('v=') + 13);
-                var youtubeUrl = 'https://www.googleapis.com/youtube/v3/videos?id=' + id + '&key=' + youtubeApiKey + '&part=snippet';
+                var youtubeId = url.substring(url.indexOf('v=') + 2, url.indexOf('v=') + 13);
+                var youtubeUrl = 'https://www.googleapis.com/youtube/v3/videos?id=' + youtubeId + '&key=' + youtubeApiKey + '&part=snippet';
                 $.ajax({
                     url: youtubeUrl,
                     dataType: 'json',
@@ -19,37 +19,30 @@ function dumpAlbums(youtubeApiKey) {
                             var title = snippet.title;
                             var defaultUrl = thumbnails.default.url;
                             var html = '';
-                            html += '<div>';
-                            html += '<table><tr><td>';
-                            html += '<button>Add Video</button>';
-                            html += '</td><td>';
+                            html += '<div class="col-xs-6 col-sm6 col-md-6 col-lg-4 playlist">';
+                            html += '<div id="' + youtubeId + '" class="playlist-area" data-title="' + title + '">';
                             html += '<img src="' + defaultUrl + '"/>';
-                            html += '</td></tr>';
-                            html += '<tr><td></td><td style="text-align: center;">'
-                            html += '<span>' + title + '</span>';
-                            html += '</td></tr>';
                             html += '</div>';
-                            $(html).find('button').click(function () {
-                                var pid = 'Z1S_glHFMe';
-                                $('#status').text('success add a video');
+                            html += '<div class="playlist-profile">';
+                            html += title;
+                            html += '</div>';
+                            html += '</div>';
 
-                                //$.ajax({
-                                //    url: 'http://swizzle.fm/repost/to/' + pid,
-                                //    data: {pid : pid, sid : },
-                                //    type: 'post',
-                                //    success: function () {
-                                //        alert('success!');
-                                //    }
-                                //});
-
-                            }).end().appendTo('#myvideos');
+                            $(html)
+                                .find('.playlist-area').css({'z-index': 9999})
+                                .draggable({
+                                    revert: true,
+                                    scroll: false
+                                }).end()
+                                .find('img').width('100%').end()
+                                .appendTo('#myvideos > .items > .row');
                         }
                     }
                 });
             }
         }
         if (!exist) {
-            $('#myvideos').append('No Video Available.');
+            $('#myvideos').append('<li>No videos available.</li> <li><a href="http://youtube.com">Go Youtube to find new videos!</a></li>');
         }
     });
 }
@@ -69,6 +62,16 @@ document.addEventListener('DOMContentLoaded', function () {
     readTextFile(youtube_apikey_filename, function (youtubeApiKey) {
         dumpAlbums(youtubeApiKey);
     });
+    $(document).on("click", "a", function () {
+        var innerUrl = $(this).attr('href');
+        if (innerUrl.indexOf('http://') > -1 || innerUrl.indexOf('https://') > -1) {
+            chrome.tabs.create({url: innerUrl});
+        } else {
+            chrome.tabs.create({url: 'http://swizzle.fm' + innerUrl});
+        }
+        return false;
+    });
+
     $.ajax({
         url: 'http://swizzle.fm/discovers/popular',
         dataType: 'html',
@@ -76,18 +79,71 @@ document.addEventListener('DOMContentLoaded', function () {
             var hrefUrl = $(html).find('.my-navi > a').attr('href');
             if (hrefUrl) {
                 var userId = hrefUrl.substring(hrefUrl.lastIndexOf('/') + 1);
-                $.ajax({
-                    url: 'http://swizzle.fm/pages/userpage/' + userId,
-                    crossDomain: true,
-                    dataType: 'html',
-                    success: function (html) {
-                        $('#albums').html(html).find('.userpage img').imgs2CacheFF();
-                    }
+
+                $(document).on("click", ".edit-user-info", function () {
+                    chrome.tabs.create({url: 'http://swizzle.fm/users/profile/' + userId});
+                    return false;
                 });
+
+                loadMyAlbums(userId);
+
             } else {
-                $('#status').append('Need to login');
+                $('#albums').append('You need to sign-in on the web. <br/><a href="/">Go swizzle for sign-in!</a>')
+                    .css('padding', 30);
             }
         }
     });
 });
 
+var loadMyAlbums = function (userId) {
+    $.ajax({
+        url: 'http://swizzle.fm/pages/userpage/' + userId,
+        crossDomain: true,
+        dataType: 'html',
+        success: function (html) {
+            $('#albums').html(html).parent()
+                .find('.playlist-area, #myvideos').droppable({
+                    hoverClass: 'ui-state-hover',
+                    greedy: true,
+                    drop: function (event, ui) {
+                        if (event.pageY > $('#myvideos').offset().top) {
+
+                        } else {
+                            var youtubeId = ui.draggable[0].id;
+                            var youtubeTitle = $(ui.draggable[0]).data('title');
+                            var playlistDom = $($(event.target).find('a')[0]);
+                            var playlistUrl = playlistDom.attr('href');
+                            var caption = $(event.target).find('.caption').text();
+                            var playlistId = playlistUrl.substring(playlistUrl.lastIndexOf('/') + 1);
+
+                            $.ajax({
+                                url: 'http://swizzle.fm/songs/create/' + playlistId,
+                                data: {
+                                    song: {
+                                        youtube_id: youtubeId,
+                                        title: youtubeTitle
+                                    },
+                                    pid: playlistId,
+                                    uid: userId
+                                },
+                                type: 'post',
+                                success: function () {
+                                    $('#status').text('success add a video [' + youtubeTitle + '] to album [' + caption + ']')
+                                        .fadeIn().delay(3000).fadeOut();
+
+                                    loadMyAlbums(userId);
+                                },
+                                error: function () {
+                                    $('#status').text('Network error!')
+                                        .fadeIn().delay(3000).fadeOut();
+                                }
+                            });
+
+
+                        }
+                    }
+                }).end()
+                .find('.userpage img').imgs2CacheFF();
+        }
+    });
+};
